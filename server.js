2fb78a062af34e2b1f97ef573c50c4b018757ef0
John Leintz <johnleintz@scaledflow.com>
2020-07-13 10:22:08 -0500

    added server file and packages

---
+++ server.js
@@ -0,0 +1,42 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// // Import routes and give the server access to them.
+// var routes = require("./controllers/burgers_controller.js");
+
+// // console.log("rountes");
+// // console.log(routes);
+
+// app.use(routes);
+
+
+
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
