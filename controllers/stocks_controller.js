a32cc58cad59b0fad0ba7753e1647a614ef585de
John Leintz <johnleintz@scaledflow.com>
2020-07-13 13:56:41 -0500

    added handlebars skeleton

--- controllers/stocks_controller.js
+++ controllers/stocks_controller.js
@@ -4,11 +4,37 @@ const express = require("express");
 const router = express.Router();
 const stock = require("../models/stock.js");
 
+var stocks = [
+  {
+    stockTicker: "F",
+    stockPrice: "6",
+  },
+  // {
+  //   stockTicker: "TSLA",
+  //   stockPrice: "1689",
+  // },
+];
+
+
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
     console.log("-- stock_controller - router.get /");
+    console.log(stocks);
 
-    //res.render("index", "test");
+    res.render("index", stocks);
+  });
+
+
+  router.get("/stock", function (req, res) {
+    console.log("-- stock_controller - router.get /stock");
+    // console.log(stocks);
+    // Handlebars requires an object to be sent to the dog handlebars file.
+    // Lucky for us, animals[0] is an object!
+  
+    // 1. send the dog object from the animals array to the dog handlebars file.
+    console.log(stocks[0].stockTicker);
+    
+    res.render("stock", stocks[0]);
   });
 
 module.exports = router;
